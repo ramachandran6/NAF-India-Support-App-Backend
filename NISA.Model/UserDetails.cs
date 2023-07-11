7b317a2bccaacb0654f4562ca77c68fc359ef2a4
elanchikk <ellanchikkumar@gmail.com>
2023-07-11 11:08:22 +0530

    user details

---
+++ NISA.Model/UserDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace NISA.Model
+{
+    public class UserDetails
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }    
+        public string name { get; set; }
+        public string email { get; set; }   
+        public string password { get; set; }    
+        public string department { get; set; }  
+        public bool isActive { get; set; }
+        public bool phoneNumber { get; set; }
+
+    }
+}
