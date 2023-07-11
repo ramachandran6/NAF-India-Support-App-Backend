4fc6c6824b2134c69bad001136a3f5ddf4d1d157
ramachandran6 <ramachandranrajappan@gmail.com>
2023-07-11 11:01:53 +0530

    DB context

---
+++ NISA.DataAccessLayer/dbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NISA.Model;
+
+namespace NISA.DataAccessLayer
+{
+    public class dbContext:DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+        public DbSet<TicketDetails> ticketDetails { get; set; }
+    }
+}
