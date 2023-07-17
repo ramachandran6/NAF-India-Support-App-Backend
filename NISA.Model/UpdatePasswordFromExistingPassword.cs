e4e0b6fa4eaa1dbc7d2fee237254ef3fb80614ba
elanchikk <ellanchikkumar@gmail.com>
2023-07-17 14:41:12 +0530

    added update password from existing password class

---
+++ NISA.Model/UpdatePasswordFromExistingPassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NISA.Model
+{
+    public class UpdatePasswordFromExistingPassword
+    {
+        public string oldPassword { get; set; }
+        public string newPassword { get; set; }
+        public string confirmNewPassword { get; set; }
+    }
+}
