d0c2fefbb38b523b98ea50be69d1db107df95b05
elanchikk <ellanchikkumar@gmail.com>
2023-07-20 12:23:14 +0530

    added image entity class

---
+++ NISA.Model/ImageEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NISA.Model
+{
+    public class ImageEntity
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string ContentType { get; set; }
+
+        [Required]
+        public byte[] ImageData { get; set; }
+
+        public bool? isActive { get; set; }
+        public int? ticketId { get; set; }
+
+        public string? uploadedDate { get; set; }
+
+    }
+}
